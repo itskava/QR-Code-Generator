3b37a5f7ecccef9a9cf656863944ceea8478c008
Timofey Tagaev <timofey.tagaev.2004.12@gmail.com>
2024-01-06 21:18:55 +0700

    Added Configuration class and EncodingType enum that I forgot to add into the last commit

---
+++ Configuration.cs
@@ -0,0 +1,10 @@
+﻿namespace QR_Code_Generator
+{
+    /// <summary>
+    /// This class is responsible for the configuration of the QR-code.
+    /// </summary>
+    internal static class Configuration
+    {
+        public static EncodingType EncodingType { get; set; } = EncodingType.Byte;
+    }
+}
