6f4c455a00f75840f4cb029bfbd41709d221ddc6
Timofey Tagaev <timofey.tagaev.2004.12@gmail.com>
2024-01-10 13:01:21 +0700

    Added CombiningBlocks class (5)

---
+++ CombiningBlocks.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace QR_Code_Generator
+{
+    /// <summary>
+    /// This class is responsible for combining data blocks with correction blocks
+    /// </summary>
+    internal static class CombiningBlocks
+    {
+        /// <summary>
+        /// This method is used to combine data blocks and correction blocks into a single bit sequence
+        /// </summary>
+        public static void Combine()
+        {
+            StringBuilder finalSequence = new StringBuilder();
+
+            int maxDataBlockLength = SeparationIntoBlocks.Blocks[^1].Length;
+            int blocksAmount = SeparationIntoBlocks.Blocks.Length;
+
+            /* Combining all data blocks by taking the first byte from each block, then the second 
+             * byte, and so on until no bytes remain. If there are no bytes in the current block, it is skipped. */
+            for (int i = 0; i < maxDataBlockLength; i += 8)
+            {
+                for (int j = 0; j < blocksAmount; ++j)
+                {
+                    string currentBlock = SeparationIntoBlocks.Blocks[j];
+
+                    if (currentBlock.Length > i) // If there are bytes in the current block
+                    {
+                        finalSequence.Append(currentBlock[i..(i + 8)]);
+                    }
+                }
+            }
+
+            int maxCorrectionBlockLength = CorrectionBytesCreation.CorrectionBlocks[^1].Length;
+            int correctionBlocksAmount = CorrectionBytesCreation.CorrectionBlocks.Length;
+
+            // Correction blocks are combined in the same way as data blocks
+            for (int i = 0; i < maxCorrectionBlockLength; i += 8)
+            {
+                for (int j = 0; j < correctionBlocksAmount; ++j)
+                {
+                    string currentBlock = CorrectionBytesCreation.CorrectionBlocks[j];
+
+                    if (currentBlock.Length > i) // If there are bytes in the current block
+                    {
+                        finalSequence.Append(currentBlock[i..(i + 8)]);
+                    }
+                }
+            }
+
+            Configuration.BitSequence = finalSequence.ToString();
+        }
+    }
+}
